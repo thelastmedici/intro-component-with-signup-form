28ac25dab9a5b1cf2180633f932ad3d1cb2ff4d4
thelastmedici <christianopeyemijoshua@gmail.com>
2025-02-22 20:31:25 +0100

    added some fixes

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
-const claim = document.querySelector('button')
-claim.addEventListener('click', function (e) {
-  console.log('i got clicked')
-});
+var output = [];
+var count = 1;
+function fizzBuzz(){
+    output.push(count++);
+ if(count % 3 && count % 5 === 0){
+    console.log("FizzBuzz");   
+ }
+ else if(count % 3 === 0){
+     console.log("Fizz");
+ }
+ else if(count % 5 === 0){
+     console.log("FizzBuzz");
+ }
+ else{
+     console.log(output);
+ }
+}
+fizzBuzz(30);
+
+basit
