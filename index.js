f9446f9e38576f513aa53166f91b469333ce9d02
thelastmedici <christianopeyemijoshua@gmail.com>
2025-02-24 01:17:53 +0100

    made changes to markup

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const claimButton = document.querySelector('button')
 
 claimButton.addEventListener('submit', function (e) {
     e.preventDefault();
+    validateFname();
+    validateLname();
 })
 
 function validateFname() {
@@ -20,4 +22,25 @@ function validateFname() {
         fnameInput.style.border = '2px solid #ccc'; // Reset border
         return true;
     }
+}
+
+function validateLname() {
+    const lnameInput = document.querySelector('#lname');
+    const lnameError = document.querySelector('#lnameError');
+
+    if (lnameInput.value.trim() === "") {
+        lnameError.textContent = "Last Name cannot be empty";
+        lnameError.style.display = 'block';
+        lnameError.style.color ='red';
+        lnameInput.style.border = '2px solid red';
+        return false;
+    } else {
+        lnameError.textContent = "";
+        lnameError.style.display = 'none';
+        lnameInput.style.border = '2px solid #ccc';
+        return true;
+    }
+}
+function validateEmail(){
+   
 }
